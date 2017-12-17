607d69a4ba5507c2bdcad9cb5f8a6be0f382c2fd
rakeshsagar64 <rakeshsagar64@gmail.com>
2017-12-17 21:15:33 +0530

    Integrated cloud database

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,28 +1,44 @@
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+//three steps to connect to moongodb
+//step 1:- create a connection
+mongoose.connect("mongodb://test:test@ds163053.mlab.com:63053/todo");
+//step 2:-design a schema
+var schema = new mongoose.Schema({
+  item:String
 
-
+});
+//step 3:-create a model
+var Model=mongoose.model('ModelName',schema);
 
 
 var controller=function (app) {
 var data=[{item:'the first thing i forgot'},{item:'another important thing i may forget'},{item:'i don\'t remember what i forgot'}];
   console.log('controller called');
   app.get("/",function(req,res){
-
+//step 4:-perform the required operations
+    //step 4.0:-select operations
+      Model.find();
       res.render('index',{'todo':data});
 
   });
 
   app.post("/addItem",function(req,res){
-    console.log(req.body);
-    data.push(req.body);
-    console.log('post request made');
+    //step 4.1:-insert operations
+    Model({item:req.body}).save();
+
+    //console.log(req.body);
+    //data.push(req.body);
+
+
+    console.log(req.body +" was saved");
 
   });
 
   app.delete("/todo/:item",function(req,res){
-
-    console.log("delete called");
+    Model.find({item:req.params.item.replace(/\-/g," ")}).remove();
+    console.log("deleted entry "+req.params.item);
 
   });
 
