6ae21e033b37b2b973d05144781f1cdc17b272b8
rakeshsagar64 <rakeshsagar64@gmail.com>
2017-12-18 11:20:07 +0530

    the basic functionality completed

--- controller/todoController.js
+++ controller/todoController.js
@@ -5,40 +5,54 @@ const mongoose = require('mongoose');
 //step 1:- create a connection
 mongoose.connect("mongodb://test:test@ds163053.mlab.com:63053/todo");
 //step 2:-design a schema
+
 var schema = new mongoose.Schema({
-  item:String
+  todo:String
 
 });
+
+var urlencoder=bodyParser.urlencoded({extends:false});
+
+
 //step 3:-create a model
-var Model=mongoose.model('ModelName',schema);
+var Model=mongoose.model('Model',schema);
 
 
 var controller=function (app) {
-var data=[{item:'the first thing i forgot'},{item:'another important thing i may forget'},{item:'i don\'t remember what i forgot'}];
+//var data=[{item:'the first thing i forgot'},{item:'another important thing i may forget'},{item:'i don\'t remember what i forgot'}];
   console.log('controller called');
   app.get("/",function(req,res){
 //step 4:-perform the required operations
     //step 4.0:-select operations
-      Model.find();
-      res.render('index',{'todo':data});
+    Model.find({},function(err,data){
 
+        if(err)throw err;
+        console.log(data);
+        res.render('index',{'todo':data});
+
+    });
   });
 
-  app.post("/addItem",function(req,res){
+  app.post("/add",urlencoder,function(req,res){
     //step 4.1:-insert operations
-    Model({item:req.body}).save();
+    Model(req.body).save(function(err){
+        if(err) throw err;
+        res.json(req.body);
+    });
 
     //console.log(req.body);
     //data.push(req.body);
 
 
-    console.log(req.body +" was saved");
 
   });
 
   app.delete("/todo/:item",function(req,res){
-    Model.find({item:req.params.item.replace(/\-/g," ")}).remove();
-    console.log("deleted entry "+req.params.item);
+    Model.find({todo:req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+        if(err) throw err;
+        res.json(data);
+
+    });
 
   });
 
